0872338438e99dd8a3bc64658c58982d431a3bea
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-07-30 20:15:08 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,8 @@
-let name="python"
-let name="HBTN"
-let name
-
-if (name=python){console.log("python is fun")}
-
-elseif(name=HBTN){console.log()}
-
-else
+function print(arg1,arg2){
+  if(arg1==defined && arg2==defined){console.log(arg1 +" is "+arg2)}
+  elseif(arg1==defined && arg2!==defined){console.log(arg1 +" is "+arg2)}
+  else(arg1!==defined && arg2!==defined){console.log(arg1 +" is "+arg2)}
+}
+print("Python","fun")
+print("HBTN")
+print()
