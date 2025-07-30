e5fbaaf6bcf746a65ca44e1c5fe8b398cafe169d
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-07-30 19:38:03 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,9 +1,9 @@
 function print(arg1,arg2,){
   console.log(arg1 + " " + "is" + " "+ arg2)}
 
-if (arg1,arg2 present)
-print("Python","fun")
-elseif
-print("HBTN")
+if (arg1,arg2){print("Python","fun")}
+
+elseif(arg1){print("HBTN")}
+
 else
 print()
