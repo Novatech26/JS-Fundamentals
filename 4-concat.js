92647945ddb0107bdc9edc275efd3c2a07581a24
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-08-01 04:15:45 +0100

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,6 @@
-let a="python fun"
-let b="HBTN"
 function print(a, b) {
  console.log(a +" is " + b)
 };
-if (a!==undefined){print("Python","fun")}
-if (b!==undefined){print("HBTN")}
-else{print()}
+print("Python","fun")
+print("HBTN")
+print()
