e697dccf241fbe9c2f1a7529a580a2eb4518d7db
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-08-01 08:22:37 +0100

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,19 +1,18 @@
 let num1= 89
 console.log("My number: "+ num1);
 
-let str1= '89'
-str1=number(str1)
-console.log("My number: "+ str1);
+let score= '89'
+score=Number(score)
+console.log("My number: "+ score);
 
-let str2= -4545
-str2=number(str2)
-console.log("My number: "+ str2);
+let score= -4545
+console.log("My number: "+ score);
 
 let str3= '-4545'
-str3=number(str3)
+str3=Number(str3)
 console.log("My number: "+ str3);
 
 let str4= 'ALX'
-str4=number(str4)
+str4=Number(str4)
 console.log("My number: "+ str4);
 
