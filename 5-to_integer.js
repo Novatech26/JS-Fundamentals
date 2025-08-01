b41122488a700dda53987520e348dd6956cffe8b
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-08-01 07:41:12 +0100

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -4,3 +4,16 @@ console.log("My number: "+ num1);
 let str1= '89'
 str1=number(str1)
 console.log("My number: "+ str1);
+
+let str1= -4545
+str1=number(str1)
+console.log("My number: "+ str1);
+
+let str1= '-4545'
+str1=number(str1)
+console.log("My number: "+ str1);
+
+let str1= 'ALX'
+str1=number(str1)
+console.log("My number: "+ str1);
+
