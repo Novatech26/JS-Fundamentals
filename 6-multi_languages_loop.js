4328eee22469a1026ece93062ae1fd18532fc972
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-08-02 17:33:33 +0100

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,4 +1,4 @@
-let lang=["C is fun", "Python is cool", "Javascript is amazing"]
+let lang=["C is fun", "Python is cool", "JavaScript is amazing"]
 
   for (let i=0; i<lang.length; i++){
   console.log(lang[i])
