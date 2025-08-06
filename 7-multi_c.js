3cb46ed374f437feffa8bf1ea0e5beff74de8649
Izunna Chima-ibeh <izunnachimaibeh@gmail.com>
2025-08-06 07:14:53 +0100

    7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,5 +1,9 @@
-let text=["C is fun","C is fun","C is fun","C is fun","C is fun","C is fun"]
-for (let i=0; i<text.length; i++){
-  console.log(text[i]);
+const x = parseInt(process.argv[2])
+
+if (isNaN(x)){
+console.log("Missing number of occurances")}
+
+else {for (let i=0; i<x; i++){
+console.log("C is fun")}
 }
 
