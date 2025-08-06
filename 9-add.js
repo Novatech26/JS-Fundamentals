db9cf5e1c8f591a82e07c2457e89890bf735ff71
Izunna Chima-ibeh <izunnachimaibeh@gmail.com>
2025-08-06 19:37:08 +0100

    add add file

--- 9-add.js
+++ 9-add.js
@@ -1,9 +1,6 @@
-  
-function sum(a,b){
-  console.log(a+b);
+let a=parseInt(process.argv[2])
+let b=parseInt(process.argv[3])
+if (isNaN(a && b)){
+  console.log("This is not a number");
 }
-
-sum(4,85)\n
-sum(-4,93)\n
-sum(4)\n
-sum()
+else{console.log(a+b);}
