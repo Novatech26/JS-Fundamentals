7d0c96290871613b4b8b73d5ac356fbd81254169
Izunna Chima-ibeh <163357609+Novatech26@users.noreply.github.com>
2025-08-06 06:42:41 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,7 +1,8 @@
 for (let i=0; i<6; i++){
-  console.log("C is fun")
+  console.log(name)
 };
 
 for (let i=0; i<1; i++){
-  console.log("C is fun")
+  console.log(name)
 }
+name.forEach ("C is fun")
