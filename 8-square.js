11056da5ee495756656c77883f50642f46910d46
Izunna Chima-ibeh <izunnachimaibeh@gmail.com>
2025-08-06 20:11:54 +0100

    add square file

---
+++ 8-square.js
@@ -0,0 +1,11 @@
+let a=parseInt(process.argv[2])
+if (isNaN(a)){
+  console.log("Not a number");
+}
+else{
+  for (let i=0; i<a; i++){
+    const sum = a => a*a
+      console.log("Size " + a)
+    
+  }
+}
