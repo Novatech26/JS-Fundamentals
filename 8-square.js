56f67a4c0e53669532f1208e9f8cf5a45f90f028
Izunna Chima-ibeh <izunnachimaibeh@gmail.com>
2025-08-06 20:21:31 +0100

    modify

--- 8-square.js
+++ 8-square.js
@@ -1,11 +1,6 @@
-let a=parseInt(process.argv[2])
-if (isNaN(a)){
-  console.log("Not a number");
-}
-else{
+let a=process.argv[2]
+
   for (let i=0; i<a; i++){
     const sum = a => a*a
-      console.log("Size " + a)
-    
+      console.log(a)
   }
-}
