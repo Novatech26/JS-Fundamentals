18e5bc65a40badc1f45d161c648925943647a224
Izunna Chima-ibeh <izunnachimaibeh@gmail.com>
2025-08-07 18:15:44 +0100

    update

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,7 +1,9 @@
 let x=parseInt(process.argv[2])
+let a=parseInt(process.argv[3])
 if (isNaN(x)){
   console.log(1)
 } else {
-  const x=()=>x*x
-  console.log(x-1)
+  function sum(){
+  console.log(x-1)}
 }
+sum()
